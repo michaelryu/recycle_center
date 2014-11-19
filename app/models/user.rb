5ec40633735121cdcc1a52a63af211b8076fdd76
michaelryu <michaelryu0@gmail.com>
2014-11-18 16:22:57 -0800

    finished editting users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+
 	attr_accessor :remember_token
 	before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
@@ -7,7 +8,7 @@ class User < ActiveRecord::Base
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
 	
 	def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
