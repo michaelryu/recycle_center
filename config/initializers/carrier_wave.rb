f4743a061cb1e149dda23406dc7d695ea148010b
Michael Ryu <michaelryu0@gmail.com>
2014-12-05 16:10:49 -0800

    working with dokku

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
 			# Configuration for Amazon S3
 			:provider              => 'AWS',
 			:aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-			:aws_secret_access_key => ENV['AWS_SECRET_KEY_KEY']
+			:aws_secret_access_key => ENV['AWS_SECRET_KEY']
 		}
 		config.fog_directory     =  ENV['S3_BUCKET']
 	end
